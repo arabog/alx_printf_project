a38c6ecd6f209120fed8342f5a3e2e5f2b448258
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2022-10-16 23:52:30 -0700

    created a test_file folder and moved files to it

--- test_files/_printf0.c
+++ test_files/_printf0.c
@@ -1,37 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - prints a character
- * @c: character
- *
- * Return: integer in character form
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_number - prints an integer
- * @n: integer to print
- */
-
-void print_number(int n)
-{
-	unsigned int n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n1;
-	}
-	if ((n1 / 10) > 0)
-		print_number(n1 / 10);
-	_putchar((n1 % 10) + '0');
-}
-
-/**
  * _printf - prints a formatted string
  * @format: pointer to character string
  *
@@ -40,7 +9,7 @@ void print_number(int n)
 
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0, flag, j, dec;
+	int i = 0, len = 0, flag, j;
 	va_list list;
 	char *str;
 	char ch;
@@ -72,16 +41,6 @@ int _printf(const char *format, ...)
 							write(1, &str[j], 1);
 							len += 1;
 						}
-						flag = 0;
-						break;
-					case 'd':
-						dec = va_arg(list, int);
-						print_number(dec);
-						flag = 0;
-						break;
-					case 'i':
-						dec = va_arg(list, int);
-						print_number(dec);
 						flag = 0;
 						break;
 				}
